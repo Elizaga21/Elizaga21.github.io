a25dae5e7b1a1bc32b667c8f6fc1e8db7985827b
Elisabet <elisabetaudiovisual@gmail.com>
2023-12-29 12:19:24 +0100

    footer y header hechos

--- header.php
+++ header.php
@@ -4,17 +4,17 @@
       <img src="/logo/logo.svg" alt="Logo de Miniaturas y Colecciones">
     </a>
   </div>
+  
+  <div class="nombre-tienda">
+    <h1>Miniaturas y Colecciones</h1>
+  </div>
 
   <div class="header-content">
-    <div class="nombre-tienda">
-      <h1>Miniaturas y Colecciones</h1>
-    </div>
-
     <nav id="menu-navegacion">
       <ul>
-        <li><a href="login.php">Iniciar Sesión</a></li>
-        <li><a href="quienes_somos.php">Quiénes Somos</a></li>
-        <li><a href="contacto.php">Contacto</a></li>
+        <li><a href="login.php"><strong>Iniciar Sesión</strong></a></li>
+        <li><a href="quienes_somos.php"><strong>Quiénes Somos</strong></a></li>
+        <li><a href="contacto.php"><strong>Contacto</strong></a></li>
       </ul>
     </nav>
   </div>
