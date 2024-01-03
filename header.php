9d6824f6f67c063deebc3d539a721c76fbe83b16
Elisabet <elisabetaudiovisual@gmail.com>
2024-01-04 00:31:35 +0100

    header personalizado para admin

--- header.php
+++ header.php
@@ -1,4 +1,3 @@
-
 <header>
   <div class="logo">
     <a href="index.php">
@@ -13,10 +12,38 @@
   <div class="header-content">
     <nav id="menu-navegacion">
       <ul>
-        <li><a href="login.php"><strong>Iniciar Sesión</strong></a></li>
-        <li><a href="quienes_somos.php"><strong>Quiénes Somos</strong></a></li>
-        <li><a href="contacto.php"><strong>Contacto</strong></a></li>
+        <?php
+        if (isset($_SESSION['user_id'])) {
+            $rolUsuario = $_SESSION['rol'];
+
+            switch ($rolUsuario) {
+                case 'administrador':
+                    echo '<li><a href="informes.php"><strong>Informes</strong></a></li>';
+                    echo '<li><a href="estadisticas.php"><strong>Estadísticas</strong></a></li>';
+                    echo '<li><a href="productos.php"><strong>Productos</strong></a></li>';
+                    echo '<li><a href="ventas.php"><strong>Ventas</strong></a></li>';
+                    break;
+                case 'empleado':
+                    // Opciones específicas para empleados
+                    break;
+                case 'cliente':
+                    // Opciones específicas para clientes
+                    break;
+                // Puedes agregar más casos según sea necesario
+
+                default:
+                    // Opciones por defecto o manejo de error
+                    break;
+            }
+        } else {
+            // Menú predeterminado para usuarios no autenticados
+            echo '<li><a href="login.php"><strong>Iniciar Sesión</strong></a></li>';
+            echo '<li><a href="quienes_somos.php"><strong>Quiénes Somos</strong></a></li>';
+            echo '<li><a href="contacto.php"><strong>Contacto</strong></a></li>';
+        }
+        ?>
       </ul>
     </nav>
   </div>
 </header>
+
